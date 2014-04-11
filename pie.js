68f80250b8d1954f134de839ace1fa2b522910ad
Phillip Quiza <pquiza@gmail.com>
2014-04-11 15:02:31 -0400

    Smaller images

--- pie.js
+++ pie.js
@@ -4,6 +4,6 @@ var page = webPage.create();
 page.viewportSize = { width: 1920, height: 1200 };
 page.zoomFactor = 2;
 page.open("http://localhost:8080/pie_viz", function start(status) {
-      page.render('pie_viz.png', {format: 'png', quality: '100'});
+      page.render('pie_viz.gif', {format: 'gif', quality: '100'});
         phantom.exit();
 });
--- test.js
+++ test.js
@@ -4,6 +4,6 @@ var page = webPage.create();
 page.viewportSize = { width: 960, height: 500 };
 page.zoomFactor = 2;
 page.open("http://localhost:8080/viz", function start(status) {
-      page.render('viz.png', {format: 'png', quality: '100'});
+      page.render('viz.gif', {format: 'gif', quality: '100'});
         phantom.exit();
 });
