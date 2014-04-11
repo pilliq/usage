98efeb95de17cd76a0c474c6ab8fac687eadebf4
Phillip Quiza <pquiza@gmail.com>
2014-04-11 15:05:30 -0400

    pics

--- pie.js
+++ pie.js
@@ -1,7 +1,7 @@
 var webPage = require('webpage');
 var page = webPage.create();
 
-page.viewportSize = { width: 1920, height: 1200 };
+page.viewportSize = { width: 1600, height: 1200 };
 page.zoomFactor = 2;
 page.open("http://localhost:8080/pie_viz", function start(status) {
       page.render('pie_viz.gif', {format: 'gif', quality: '100'});
