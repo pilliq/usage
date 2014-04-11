3fb292e2fa75cf6e63c4eaa443c51e0f13c9e99d
Phillip Quiza <pquiza@gmail.com>
2014-04-11 12:45:52 -0400

    Added phantomjs render tests

---
+++ test.js
@@ -0,0 +1,9 @@
+var webPage = require('webpage');
+var page = webPage.create();
+
+page.viewportSize = { width: 960, height: 500 };
+page.zoomFactor = 2;
+page.open("http://localhost:8080/viz", function start(status) {
+      page.render('viz.png', {format: 'png', quality: '100'});
+        phantom.exit();
+});
