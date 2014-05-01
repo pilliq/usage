b3b947b519c93484147f033f8ec9100017929534
Phillip Quiza <pquiza@gmail.com>
2014-05-01 11:38:22 -0400

    Update line graph phantom script

--- line.js
+++ line.js
@@ -3,7 +3,7 @@ var page = webPage.create();
 
 page.viewportSize = { width: 960, height: 500 };
 page.zoomFactor = 2;
-page.open("http://localhost:8080/line_viz", function start(status) {
+page.open("http://localhost:8000/line_viz", function start(status) {
       page.render('static/img/line_viz.gif', {format: 'gif', quality: '100'});
         phantom.exit();
 });
