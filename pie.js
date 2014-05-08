2004f94dbe3bbb79f6281c1c7711b4a909da14c8
Phillip Quiza <pquiza@gmail.com>
2014-05-08 14:10:11 -0400

    /static/img updates

--- pie.js
+++ pie.js
@@ -3,7 +3,7 @@ var page = webPage.create();
 
 page.viewportSize = { width: 1600, height: 1200 };
 page.zoomFactor = 2;
-page.open("http://localhost:8080/pie_viz", function start(status) {
-      page.render('pie_viz.gif', {format: 'gif', quality: '100'});
+page.open("http://localhost:8000/pie_viz", function start(status) {
+      page.render('pie_viz.tmp.gif', {format: 'gif', quality: '100'});
         phantom.exit();
 });
