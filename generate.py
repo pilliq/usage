913d64e4c9e2bc83b6c051ed96d2ef88891e1442
Phillip Quiza <pquiza@gmail.com>
2014-05-09 11:05:00 -0400

    Modified bar graph, removed number.gif

--- generate.py
+++ generate.py
@@ -5,11 +5,10 @@ phantom_cmd = 'phantomjs'
 phantom_script = 'render_graph.js'
 
 graphs = (
-    [phantom_cmd, phantom_script, 'http://localhost:8000/viz', 'static/img/viz.gif', '1600', '1200', '3'],
+    [phantom_cmd, phantom_script, 'http://localhost:8000/viz', 'static/img/viz.gif', '960', '500', '2'],
     [phantom_cmd, phantom_script, 'http://localhost:8000/line_viz', 'static/img/line_viz.gif', '960', '500', '2'],
     [phantom_cmd, phantom_script, 'http://localhost:8000/pie_viz', 'static/img/pie_viz.gif', '1600', '1200', '2'],
     [phantom_cmd, phantom_script, 'http://localhost:8000/stack', 'static/img/stack.gif', '1600', '1200', '3'],
-    [phantom_cmd, phantom_script, 'http://localhost:8000/numbers', 'static/img/numbers.gif', '1600', '1200', '2'],
 )
 
 if __name__ == '__main__':
